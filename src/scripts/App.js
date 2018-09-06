1227dd099c499e8c8188031870ffb54ba70dbad5
Erin <erin_maas@yahoo.com>
2018-09-06 17:08:17 -0400

    added authentication to app.js

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -7,11 +7,29 @@ import LandingPage from "./LandingPage";
 import Finder from "../components/Finder";
 
 class App extends Component {
+  constructor(){
+    super();
+      this.state = {
+        user: null
+      }
+    }
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        // this is where we'll make our firebase ref call
+        // to display users search results / saved recipes / notes
+      }
+      
+    })
+  }
+  setUser = (userId) => {
+    console.log(userId);
+  }
   render() {
     return (
       <Router>
         <div className="App">
-          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/" component={(props) => <LandingPage {...props} setUser = {this.setUser} />}/>
 
           <Route exact path="/finder" component={Finder}/>
         </div>
