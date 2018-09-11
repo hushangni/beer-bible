ea57cc9668a881ff1332d41c5eb82f2efcbc352f
Stefan <stefan.f.miranda@gmail.com>
2018-09-11 12:41:57 -0400

    yummly styled

--- src/components/GetFoodRecipes.js
+++ src/components/GetFoodRecipes.js
@@ -11,7 +11,7 @@ export default class GetFoodRecipes extends Component{
         }
     }
     foodRecommendations = (foods) => {
-
+        document.getElementById("food-button").style.display = "none";
         const yummlyID = "?_app_id=1ca882d1";
         const yummlyKey = "&_app_key=9f727f501d139e3a8cc8a2bfebddaf3a";
         const yummlyUrl = `https://api.yummly.com/v1/api/recipes${yummlyID}${yummlyKey}`;
@@ -48,15 +48,23 @@ export default class GetFoodRecipes extends Component{
 
     render(){
         return(
-            <div>
-                <button onClick={()=>this.foodRecommendations(this.props.foodPairings)}>Food</button>
-                <ul className="foodPairingRecipes">
+            <div className="food-module">
+                <button className="food-button" id="food-button" onClick={()=>this.foodRecommendations(this.props.foodPairings)}>
+                    Get Food Pairing Ideas
+                </button>
+                <ul className="foodPairingRecipes clearfix">
                 {this.state.foodRecipeDisplay.map((recipe) => {
                     return(
-                            <li>
-                                <h4>{recipe.foodName}</h4>
+                            <li className="food-item">
+                            <h4>
                                 <a href={recipe.foodRecipeId}>
-                                    <img src={recipe.foodImg} alt="" />
+                                {recipe.foodName.length>40? recipe.foodName.slice(0,40) + "...":recipe.foodName}
+                                </a></h4>
+                                <a href={recipe.foodRecipeId}>
+                                    <div className="foodImage">
+                                        <img src={recipe.foodImg} alt=""/>
+                                        <p className="foodImage-hoverText">Click for Recipe</p>
+                                    </div>
                                 </a>
                             </li>
                     )
